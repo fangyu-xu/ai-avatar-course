56e3daf2394e9685c48b82d256ec79c15c3bed17
fangyu-xu <fangyu.xu@trawa.de>
2025-02-20 15:25:17 +0100

    add ui lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+import { Dialog } from "../components/ui/dialog";
 
 export default async function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div>Create your own photo realistic AI avatars</div>
-      </main>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div>Create your own photo realistic AI avatars</div>
+      <Button className="rounded-full bg-black">
+        Create your own AI avatars now!
+      </Button>
+    </main>
   );
 }
